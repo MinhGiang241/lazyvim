e8ce7b175a622b4dcdb9ce838e4e5d703c4a71b8
Nash Truong <Nash.Truong@avepoint.com>
2024-11-27 15:19:54 +0700

    add color border

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -92,6 +92,42 @@ end, { desc = "Pick a window" })
 -- Colorscheme
 vim.cmd([[colorscheme base16-catppuccin-mocha]])
 
+vim.diagnostic.config({
+  float = {
+    border = "rounded", -- Kiểu viền: 'none', 'single', 'double', 'rounded', 'solid', 'shadow'
+  },
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded", -- Kiểu viền: 'none', 'single', 'double', 'rounded', 'solid', 'shadow'
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "rounded", -- Kiểu viền: 'none', 'single', 'double', 'rounded', 'solid', 'shadow'
+})
+
+-- vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#FFD700" })
+-- vim.api.nvim_set_hl(0, "NormalFloatBorder", { bg = "#FFD700" })
+local border = {
+  { "🭽", "FloatBorder" },
+  { "▔", "FloatBorder" },
+  { "🭾", "FloatBorder" },
+  { "▕", "FloatBorder" },
+  { "🭿", "FloatBorder" },
+  { "▁", "FloatBorder" },
+  { "🭼", "FloatBorder" },
+  { "▏", "FloatBorder" },
+}
+
+-- LSP settings (for overriding per client)
+local handlers = {
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+}
+
+-- Do not forget to use the on_attach function
+require("lspconfig").myserver.setup({ handlers = handlers })
+
 vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#FF5733" }) -- Màu viền cho toàn bộ cửa sổ Telescope
 vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#A3BE8C" }) -- Màu viền cho cửa sổ Prompt
 vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#5E81AC" }) -- Màu viền cho cửa sổ Results
@@ -100,3 +136,47 @@ vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#BF616A" }) -- Màu vi�
 vim.api.nvim_set_hl(0, "MasonNormal", { fg = "#FFFFFF", bg = "#2E3440" }) -- Màu nền cho Mason
 vim.api.nvim_set_hl(0, "MasonBorder", { fg = "#88C0D0", bg = "NONE" }) -- Màu viền cho cửa sổ Mason
 vim.api.nvim_set_hl(0, "MasonHeading", { fg = "#A3BE8C", bg = "NONE" }) -- Màu tiêu đề
+
+local cmp = require("cmp")
+cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+})
+
+-- color documentation border
+vim.api.nvim_set_hl(0, "CmpDocBorder", { fg = "#FFFFFF" })
+
+-- color menu autocompletion border
+vim.api.nvim_set_hl(0, "CmpMenuBorder", { fg = "#FFFFFF" })
+
+-- local api = vim.api
+-- local diagnostic = vim.diagnostic
+--
+-- api.nvim_create_autocmd("CursorHold", {
+--   buffer = bufnr,
+--   callback = function()
+--     local float_opts = {
+--       focusable = false,
+--       close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+--       border = "rounded",
+--       source = "always", -- show source in diagnostic popup window
+--       prefix = " ",
+--     }
+--
+--     if not vim.b.diagnostics_pos then
+--       vim.b.diagnostics_pos = { nil, nil }
+--     end
+--
+--     local cursor_pos = api.nvim_win_get_cursor(0)
+--     if
+--       (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+--       and #diagnostic.get() > 0
+--     then
+--       diagnostic.open_float(nil, float_opts)
+--     end
+--
+--     vim.b.diagnostics_pos = cursor_pos
+--   end,
+-- })
