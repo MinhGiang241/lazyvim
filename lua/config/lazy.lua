bdae0e7ee899ec57a9bb1231e44a955617f05231
Minh Giang <minhgiang241@gmail.com>
2024-11-28 22:11:04 +0700

    dap typescript

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- import/override with your plugins
     { import = "plugins" },
   },
