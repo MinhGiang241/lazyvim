d1239772d7dabe8380af419bb0289803fec03a03
Nash Truong <nash.truong@avepoint.com>
2026-09-18 10:48:27 +0700

    add mark

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -11,15 +11,15 @@ local M = {
     vim.o.foldenable = true
 
     vim.opt.fillchars = {
-      foldopen = "",
-      foldclose = "",
+      foldopen = "",
+      foldclose = "",
       fold = " ",
       foldsep = " ",
     }
 
     -- Chỉ hiển thị v/> nếu dòng hiện tại là DÒNG BẮT ĐẦU của một khối gập
     vim.o.statuscolumn =
-      '%= %l %s%{foldclosed(v:lnum) == v:lnum ? ">" : (foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? "" : " ")} '
+      '%= %l %s%{foldclosed(v:lnum) == v:lnum ? "" : (foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? "" : " ")} '
 
     vim.api.nvim_create_autocmd("FileType", {
       group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
